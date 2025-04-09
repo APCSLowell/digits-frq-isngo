6aae63f7d4e5e2df5ea173c4787a59f26992a193
isngo <isngo@s.sfusd.edu>
2025-04-09 09:48:38 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,13 +7,26 @@ public class Digits
 
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
-	    
+		str = num + "";
+		ArrayList<String> arl = new ArrayList<String>();
+		for(int i = 0; i<str.length(); i++){
+			arl.add(str.substring(i,i+1));
+		}
+		ArrayList<Integer> arl2 = ArrayList<Integer>();
+		for(int i = 0; i<arl.size(); i++){
+			arl2.add(Integer.parseInt(arl.get(i)));
+		}
+		digitList = arl2;
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		
-
+		for(int i  = 0; i<digitList.size()-1; i++){
+			if(digitList.get(i) >= digitList.get(i+1)){
+				return false;
+			}
+		}
+		return true;
 	}
 	
 	public String toString()
